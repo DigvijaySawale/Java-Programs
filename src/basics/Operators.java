c94d0ec04b6c544f95b0ad8a90e1cd1d02cf3bb6
Digvijay <sawaledigvijay70@gmail.colm>
2025-01-01 16:58:14 +0530

    Operators in java

---
+++ src/basics/Operators.java
@@ -0,0 +1,59 @@
+package basics;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+
+        //arithmetic operator
+//        int result = a + b;
+//        System.out.println(result);
+        int c = a/b;
+        double d = a/b;
+        double e = (double)a/b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        int f = a%b;
+        System.out.println(f);
+
+        //assignment operator
+
+        a+=b;
+        System.out.println(a);
+
+        //relational operator
+
+        int m = 12;
+        int n = 7;
+/*
+        System.out.println(m==n);
+        System.out.println(m!=n);
+        System.out.println(m<n);
+        System.out.println(m>n);
+        System.out.println(m<=n);
+        System.out.println(m>=n);*/
+
+
+        // logical operator
+        boolean firsEx = true;
+        boolean secondEx = false;
+/*
+        System.out.println(firsEx && secondEx);
+        System.out.println(firsEx || secondEx);
+        System.out.println(!firsEx);
+        System.out.println(!secondEx);
+        System.out.println(!(firsEx && secondEx));*/
+
+
+        // Unary/ other operator
+        int x = 12;
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+
+    }
+}
